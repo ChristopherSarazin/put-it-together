03d293cc841bd3316946a945d30c64245bbc6f03
Christopher Sarazin <84635155+ChristopherSarazin@users.noreply.github.com>
2021-09-02 10:51:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h1>Things I need to do:</h1>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default App;
